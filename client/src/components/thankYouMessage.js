6cfd0ca133b711140de797ee21c53f2f473f3931
Facu <facutotaro@gmail.com>
2024-08-26 21:29:26 -0300

    add share buttons

--- client/src/components/thankYouMessage.js
+++ client/src/components/thankYouMessage.js
@@ -1,5 +1,15 @@
 import '../pages/experiment.css';
 import { useEffect } from 'react';
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  TwitterShareButton,
+  XIcon, 
+  WhatsappShareButton,
+  WhatsappIcon, 
+  TelegramShareButton,
+  TelegramIcon
+} from 'react-share'
 
 export default function ThankYouMessage() {
   useEffect(() => {
@@ -7,10 +17,30 @@ export default function ThankYouMessage() {
     window.history.pushState(null, null, '/');
   }, []);
 
+  const shareUrl = process.env.REACT_APP_SERVER_BASE_ROUTE
+  const title = "Experimento de Neurociencia - LIAA"
+
   return (
-    <div>
+    <div className='container'>
       <h1 className='ThankYou'> ¡Gracias por participar! </h1>
-      <h2 className='BlueSubHeader'> Por favor comparte nuestro experimento </h2>
+      <h2 className='BlueSubHeader' style={{ padding: '0px' }}> Por favor comparte nuestro experimento </h2>
+      <div className='share-container'>
+        <FacebookShareButton url={shareUrl}>
+          <FacebookIcon size={50} round={true} />
+        </FacebookShareButton>
+
+        <TwitterShareButton url={shareUrl} title={title}>
+          <XIcon  size={50} round={true} />
+        </TwitterShareButton>
+
+        <WhatsappShareButton url={shareUrl} title={title}>
+          <WhatsappIcon  size={50} round={true} />
+        </WhatsappShareButton>
+
+        <TelegramShareButton url={shareUrl} title={title}>
+          <TelegramIcon  size={50} round={true} />
+        </TelegramShareButton>
+      </div>
     </div>
   );
 }

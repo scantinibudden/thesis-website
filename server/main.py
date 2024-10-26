4b3ca847b665127b9424f230c26ddf8daa79c652
Sebastian Cantini Budden <scantinibudden@gmail.com>
2024-10-26 13:53:31 -0300

    added app to main

--- server/main.py
+++ server/main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from config import API_HOST, API_PORT
+from api import app
 
 if __name__ == "__main__":
     uvicorn.run("api.app:app", host=API_HOST, port=int(API_PORT), reload=True)
